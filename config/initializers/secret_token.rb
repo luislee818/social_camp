12843ec6271992b1b35603b108af611faa945ac7
Dapeng Li <luislee818@gmail.com>
2012-04-20 14:33:21 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialCamp::Application.config.secret_token = 'b24e09ed735f946035ac72293d88d1524ddb405423d99622d617aea1a15b7ea8cb27be8a36bda063b3d105db8e8747085d6ee710f528f4dc6121d0c57314f9e1'
